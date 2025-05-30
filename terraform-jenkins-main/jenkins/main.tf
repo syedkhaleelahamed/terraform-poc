39adb5caf1b04f78ade5663c47ee1f55731db822
syedkhaleelahamed <31824248+syedkhaleelahamed@users.noreply.github.com>
2025-05-30 16:34:18 -0400

    Update main.tf

--- terraform-jenkins-main/jenkins/main.tf
+++ terraform-jenkins-main/jenkins/main.tf
@@ -1,15 +1,10 @@
 variable "ami_id" {}
 variable "instance_type" {}
 variable "tag_name" {}
-variable "public_key" {}
 variable "subnet_id" {}
 variable "sg_for_jenkins" {}
 variable "enable_public_ip_address" {}
-variable "user_data_install_jenkins" {}
 
-output "ssh_connection_string_for_ec2" {
-  value = format("%s%s", "ssh -i /Users/ksyed200@cable.comcast.com/.ssh/jekins-demo ubuntu@", aws_instance.jenkins_ec2_instance_ip.public_ip)
-}
 
 output "jenkins_ec2_instance_ip" {
   value = aws_instance.jenkins_ec2_instance_ip.id
@@ -19,27 +14,40 @@ output "dev_proj_1_ec2_instance_public_ip" {
   value = aws_instance.jenkins_ec2_instance_ip.public_ip
 }
 
+
+# Generate SSH key pair
+resource "tls_private_key" "demo_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Store private key in SSM Parameter Store
+resource "aws_ssm_parameter" "private_key" {
+  name        = "/ssh/demo-keypair/private"
+  description = "Private SSH key for EC2 demo"
+  type        = "SecureString"
+  value       = tls_private_key.demo_key.private_key_pem
+
+  tags = {
+    environment = "demo"
+  }
+}
+
+# Create AWS key pair using the public key
+resource "aws_key_pair" "demo_keypair" {
+  key_name   = "awsdevops-jenkins-demo-keypair"
+  public_key = tls_private_key.demo_key.public_key_openssh
+}
+
 resource "aws_instance" "jenkins_ec2_instance_ip" {
   ami           = var.ami_id
   instance_type = var.instance_type
   tags = {
     Name = var.tag_name
   }
-  key_name                    = "jekins-demo"
+  key_name                    = aws_key_pair.demo_keypair.key_name
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = var.sg_for_jenkins
   associate_public_ip_address = var.enable_public_ip_address
 
-  user_data = var.user_data_install_jenkins
-
-  metadata_options {
-    http_endpoint = "enabled"  # Enable the IMDSv2 endpoint
-    http_tokens   = "required" # Require the use of IMDSv2 tokens
-  }
 }
-
-resource "aws_key_pair" "jenkins_ec2_instance_public_key" {
-  key_name   = "jekins-demo"
-  public_key = var.public_key
-}
-
