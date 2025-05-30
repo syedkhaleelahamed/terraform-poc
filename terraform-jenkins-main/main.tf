1742a948281df24466fc307298486f0213843e94
syedkhaleelahamed <31824248+syedkhaleelahamed@users.noreply.github.com>
2025-05-30 11:41:14 -0400

    Update main.tf

--- terraform-jenkins-main/main.tf
+++ terraform-jenkins-main/main.tf
@@ -24,7 +24,6 @@ module "security_group" {
    subnet_id                 = tolist(module.networking.dev_proj_1_public_subnets)[0]
    sg_for_jenkins            = [module.security_group.sg_ec2_sg_ssh_http_id, module.security_group.sg_ec2_jenkins_port_8080]
    enable_public_ip_address  = true
-   user_data_install_jenkins = templatefile("./jenkins-runner-script/jenkins-installer.sh", {})
  }
 /*
 module "lb_target_group" {
