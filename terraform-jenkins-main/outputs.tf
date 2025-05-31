9652622116ed7de0798a48f7d8e494a2d714bdd5
syedkhaleelahamed <31824248+syedkhaleelahamed@users.noreply.github.com>
2025-05-30 23:54:53 -0400

    Update outputs.tf

--- terraform-jenkins-main/outputs.tf
+++ terraform-jenkins-main/outputs.tf
@@ -1,15 +1,7 @@
 output "jenkins_ec2_instance_id" {
-  value = module.jenkins.aws_instance.jenkins_ec2_instance_ip.id
-}
-
-output "private_key_pem" {
-
-  description = "Private key PEM for the EC2 instance"
-  sensitive   = true
-  value = module.jenkins.aws_ssm_parameter.private_key.value
-  
+  value = module.jenkins.instance_id
 }
 
 output "public_ip" {
-  value = module.jenkins.aws_instance.jenkins_ec2_instance_ip.public_ip
+  value = module.jenkins.instance_public_ip
 }
