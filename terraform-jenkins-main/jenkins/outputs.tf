07882641676644260175b0eae2a0d1fd2a1c0a35
syedkhaleelahamed <31824248+syedkhaleelahamed@users.noreply.github.com>
2025-05-30 23:52:36 -0400

    Update outputs.tf

--- terraform-jenkins-main/jenkins/outputs.tf
+++ terraform-jenkins-main/jenkins/outputs.tf
@@ -1,1 +1,9 @@
-
+output "instance_id" {
+  description = "ID of EC2 instance"
+  value       = aws_instance.jenkins_ec2_instance_ip.id
+}
+ 
+output "instance_public_ip" {
+  description = "Public IP of EC2 instance"
+  value       = aws_instance.jenkins_ec2_instance_ip.public_ip
+}
